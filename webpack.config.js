426c553f3dacbc9ed66f609b76c6644eaebde9ca
valley.antoine <valley.antoine@gmail.com>
2022-10-05 22:43:01 +0200

    chore: add documentation + way to release

--- webpack.config.js
+++ webpack.config.js
@@ -132,24 +132,6 @@ var options = {
     new CopyWebpackPlugin({
       patterns: [
         {
-          from: 'src/assets/img/icon-128.png',
-          to: path.join(__dirname, 'build'),
-          force: true,
-        },
-      ],
-    }),
-    new CopyWebpackPlugin({
-      patterns: [
-        {
-          from: 'src/assets/img/icon-34.png',
-          to: path.join(__dirname, 'build'),
-          force: true,
-        },
-      ],
-    }),
-    new CopyWebpackPlugin({
-      patterns: [
-        {
           from: 'src/assets/icons',
           to: path.join(__dirname, 'build/icons'),
           force: true,
